01a5446c1ef9278ed1dba2eebbedaf617e3d3aa7
Kento0206 <ken200206@icloud.com>
2024-11-07 00:17:06 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+let month = today.getMonth();
+let day = today.getDate()
+month = month + 1;
+day = day - 1;
+console.log(year + "年" + month + "月" + day + "日");
